6ca6f4420cfc37399e03644490a8f910ee90c89a
Mh-Monzil <monzil246@gmail.com>
2024-03-20 14:49:54 +0600

    navigate button added

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,16 +1,15 @@
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const User = ({user}) => {
     const {name,id} = user;
+    const navigate = useNavigate();
     return (
         <div className="card w-full md:w-96 mx-auto bg-base-100 shadow-xl border ">
             <div className="card-body flex flex-col">
                 <h2 className="text-2xl font-semibold pt-4">{name}</h2>
                 <div className="card-actions justify-end pt-8">
-                    <Link to={`/user-details/${id}`}>
-                        <button className="btn btn-error text-white text-lg w-full">See Details</button>
-                    </Link>
+                    <button onClick={() => navigate(`/user-details/${id}`)} className="btn btn-error text-white text-lg w-full">See Details</button>
                 </div>
             </div>
         </div>
--- src/Components/UserDetails.jsx
+++ src/Components/UserDetails.jsx
@@ -1,12 +1,16 @@
 
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const UserDetails = () => {
     const user = useLoaderData();
     const {name,website,email,username,phone} = user;
     console.log(user,name, website);
+    const navigate = useNavigate();
     return (
+        <>
+        <button onClick={() => navigate('/users')} className='btn btn-error text-white m-4'>Go Back</button>
         <div className=" w-full md:w-96 mx-auto bg-base-100 shadow-xl border text-center m-4 rounded-2xl">
+            
             <div className="card-body flex flex-col ">
                 <h2 className="text-2xl font-bold pt-4">Name: {name}</h2>
                 <div className='font-semibold text-lg'>
@@ -17,6 +21,7 @@ const UserDetails = () => {
                 </div>
             </div>
         </div>
+        </>
     );
 };
 
